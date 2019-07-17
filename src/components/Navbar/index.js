bef833482708326fec976a6b8fbc75e565de810c
xrole <xrolediamond@gmail.com>
2019-07-17 14:03:34 +0100

    ft(navbar): create navbar component
    create navbar component
    
    [Finishes: #167290215]

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,37 +6,32 @@ export class Navbar extends Component {
     super();
   }
 
-  render() {
+  render () {
     return (
-      <Fragment>
-        <nav className="nav-con" id="nav-con">
-          <div className="navbar navbar-expand-lg">
-            <a className="navbar-brand" > Blue FI</a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="nabarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"> </span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active"><a className="nav-link" > About </a></li>
-                <li className="nav-item active"><a className="nav-link" > Testimonies </a></li>
-                <li className="nav-item active"><a className="nav-link"> How it works </a></li>
-              </ul>
-            </div>
-            <div>
-              <p>Place Order </p>
-            </div>
+      <div className="navbar-container">
+        <nav className="navbar navbar-expand-lg navbar-light nav-section">
+          <a className="navbar-brand" href="#" id="brand">Blue-FI</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item disable">
+                <a class="nav-link highlight-on-hover" href="#">About <span class="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item disable">
+                <a className="nav-link highlight-on-hover" href="#">Testimonies</a>
+              </li>
+              <li className="nav-item disable">
+                <a className="nav-link highlight-on-hover" href="#">How it works</a>
+              </li>
+            </ul>
+            <p className="highlight-on-hover link disable order"> Place Order</p>
           </div>
         </nav>
-      </Fragment>
-    );
+      </div>
+
+    )
   }
 }
 

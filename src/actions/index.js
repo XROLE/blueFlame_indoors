938a4120e707f1073181ab6a7332493698a5be9a
xrole <xrolediamond@gmail.com>
2019-09-28 15:16:35 +0100

    save token in local storage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
+import {
+  SIGNUP_IN_PROGRESS,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILURE
+} from '../actions/actionType';
 
 export const signUpAction = (userData) => (dispatch) => {
-  console.log('I am a chosen one. comming sooooooooooo ', userData);
-
-  const signedUpUser = axios.post(
+  dispatch({ type: SIGNUP_IN_PROGRESS })
+  return axios.post(
     "https://blueflame-indoors-api.herokuapp.com/api/v1/users", userData
   )
   .then((data) => {
     console.log(data.data);
-    return data.data;
+    localStorage.setItem('BFT-token', data.data.Token);
+    return dispatch({
+      type: SIGNUP_SUCCESS,
+      user: data.data
+    })
   })
-  .catch(error => console.log("This is the error from axios ", error))
-  return dispatch({
-    type: 'TEST_ACTION',
-    userData,
+  .catch(error => {
+    console.log("This is the error from axios ", error);
+    return dispatch({
+      type: SIGNUP_FAILURE,
+      error
+    })
   })
 }

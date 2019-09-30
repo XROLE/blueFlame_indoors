08e7ef6d4f67321f2a2d9a15cd1231e7774b9934
xrole <xrolediamond@gmail.com>
2019-09-30 14:40:58 +0100

    add spinner to landing page

---
+++ src/components/AppSpinnerContainer/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Spinner from '../Spinner';
+import './appSpinnerContainer.scss';
+
+export default () => (
+  <div className="app-spinner-container">
+    <Spinner />
+  </div>
+)

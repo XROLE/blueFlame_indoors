878604283387492926bc8e0e97acf76f5adb9686
xrole <xrolediamond@gmail.com>
2019-10-02 19:01:51 +0100

    add side bar

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './sidebar.scss';
+
+export default () => {
+  return(
+    <div className="sidebar">
+      <div>
+        <h6 className="sidebar-header">CURTAIN</h6>
+        <p>Bail Design</p>
+        <p>Rich Curtain</p>
+        <p>Gold Cost</p>
+        <p>Rear Wax</p>
+        <p>Sailing Boat</p>
+        <p>More....</p>
+      </div>
+      <div>
+        <h6 className="sidebar-header">BLIND</h6>
+        <p>Bail Design</p>
+        <p>Rich Curtain</p>
+        <p>Gold Cost</p>
+        <p>Rear Wax</p>
+        <p>Sailing Boat</p>
+        <p>More....</p>
+      </div>
+      <div>
+        <h6 className="sidebar-header">ACCESORIES</h6>
+        <p>Bail Design</p>
+        <p>Rich Curtain</p>
+        <p>Gold Cost</p>
+        <p>Rear Wax</p>
+        <p>Sailing Boat</p>
+        <p>More....</p>
+      </div>
+    </div>
+  )
+}

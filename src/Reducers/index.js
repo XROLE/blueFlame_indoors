6834840956e03a8c0b510ea941ac0dd6ad74f563
xrole <xrolediamond@gmail.com>
2020-01-28 07:23:52 +0100

    fetch and display products

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import signUpReducer from './signUpReducer';
+import products from './productReducer';
 
 export const rootReducer = combineReducers({
-  signUp: signUpReducer
+  signUp: signUpReducer,
+  products
 });
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,17 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { productsData } from './productsData';
+import { connect } from 'react-redux';
+import { getAllProducts } from '../../actions';
 import './product.scss';
 
-export default class Products extends Component {
+export class Products extends Component {
+  componentDidMount(){
+    this.props.getAllProducts();
+  };
+
   render(){
     return(
       <div className="row product-container">
-        {productsData.map(product => (
-          <div className="col-6 col-md-6 col-lg-4 card-container" key={productsData.id} >
+        {this.props.products.products.map(product => (
+          <div className="col-6 col-md-6 col-lg-4 card-container" key={product.id} >
             <Link to='/item' className="card">
               <div className="card-body ">
-                <div className="image-container" style={{backgroundImage: "url(" + (product.imageUrl) + ")"}}></div>
+                <div className="image-container" style={{backgroundImage: "url(" + (product.image) + ")"}}></div>
                 <div className="cart-section mt-2">
                   <p> #{product.price}</p>
                   <p className="cart-link">+ To Cart</p>
@@ -24,3 +29,15 @@ export default class Products extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  products: state.products,
+});
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getAllProducts: () => dispatch(getAllProducts())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
+
